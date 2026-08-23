d566ab8534cf35aad497880f429b53d1be6f0a9d
YueDJ <22331182+YueDJ@users.noreply.github.com>
2026-08-23 21:23:05 +0800

    refactor(agent-host): remove obsolete external Pi process and control server

--- packages/agent-host/src/index.ts
+++ packages/agent-host/src/index.ts
@@ -1,11 +1,9 @@
-export * from './rpc-client.js';
-export * from './process.js';
-export { ControlServer } from './control-server.js';
-export * from './bridge/typebox.js';
-export * from './workflow/types.js';
-export * from './workflow/linear.js';
+export * from "./rpc-client.js";
 export * from "./pi-runtime-transport.js";
 export * from "./pi-runtime-supervisor.js";
 export * from "./pi-runtime.js";
 export * from "./pi-runtime-tools.js";
 export * from "./pi-sdk-runtime.js";
+export * from "./bridge/typebox.js";
+export * from "./workflow/types.js";
+export * from "./workflow/linear.js";
