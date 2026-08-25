f19171907d75ba4468e1ce969085687ed4af7ede
YueDJ <22331182+YueDJ@users.noreply.github.com>
2026-08-25 23:19:44 +0800

    chore: remove dev launcher script

--- start-sparkii.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Sparkii Desktop dev launcher - double-click to open the app window
-set "SPARKII_DATA_DIR=%APPDATA%\Sparkii\data"
-cd /d "C:\Users\YDJ\Desktop\SparkiiDesktop-Pi\apps\desktop"
-start "" "C:\Users\YDJ\Desktop\SparkiiDesktop-Pi\node_modules\.pnpm\electron@43.4.1_supports-color@7.2.0\node_modules\electron\dist\electron.exe" .
